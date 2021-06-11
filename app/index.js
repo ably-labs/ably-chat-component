910c6a5403017e969af62aa59c263dbd2607782d
thisisjofrank <jofranchetti@gmail.com>
2021-06-11 14:20:44 +0100

    Tweaking packaging.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,1 @@
-export * from "./component/AblyBaseComponent";
-export * from "./component/AblyChatComponent";
-export default { AblyBaseComponent, AblyChatComponent };
+export { AblyChatComponent } from "./component/AblyChatComponent";
